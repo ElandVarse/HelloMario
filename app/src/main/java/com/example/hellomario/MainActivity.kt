1e214594484f3c9f774029b423526580d0b17cab
André Pereira <andre.jobs23@gmail.com>
2025-05-06 20:48:15 -0300

    Kirby clickable moving

--- app/src/main/java/com/example/hellomario/MainActivity.kt
+++ app/src/main/java/com/example/hellomario/MainActivity.kt
@@ -5,23 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.hellomario.ui.theme.HelloMarioTheme
+
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +38,7 @@ class MainActivity : ComponentActivity() {
             HelloMarioTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Mario",
+                        /*name = "Kirby",*/
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -41,32 +48,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Hello $name!",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
+fun Greeting(modifier: Modifier = Modifier) {
+    val screenWidth = LocalConfiguration.current.screenWidthDp
+    val screenHeight = LocalConfiguration.current.screenHeightDp
 
+    var offsetX by remember { mutableStateOf(0.dp) }
+    var offsetY by remember { mutableStateOf(0.dp) }
+
+    val imageSize = 200.dp
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFB3E5FC))
+    ) {
+        fun moveKirbyRandomly() {
+            val maxX = screenWidth - imageSize.value.toInt()
+            val maxY = screenHeight - imageSize.value.toInt()
+
+            val randomX = (0..maxX).random().dp
+            val randomY = (0..maxY).random().dp
+
+            offsetX = randomX
+            offsetY = randomY
+        }
         Image(
-            painter = painterResource(id = R.drawable.mario),
-            contentDescription = "Mario Nintendo",
-            modifier = Modifier.size(400.dp),
+            painter = painterResource(id = R.drawable.kirby),
+            contentDescription = "Kirby pulante",
+            modifier = Modifier
+                .size(imageSize)
+                .offset(x = offsetX, y = offsetY)
+                .clickable (
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() }
+                ){ moveKirbyRandomly() },
             contentScale = ContentScale.Fit
         )
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HelloMarioTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
